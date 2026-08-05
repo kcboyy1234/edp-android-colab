65d1c7480fa96629ef8a5e50bda36ab1e337e0ee
Kyle Christian Gonzales <kcgonzales78193@liceo.edu.ph>
2026-08-05 09:47:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme
+val PrimaryLight = Color(0xFF00897B)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFB2DFDB)
+val SecondaryLight = Color(0xFF26A69A)
+val SurfaceLight = Color(0xFFF7FCFC)
+val OnSurfaceVariantLight = Color(0xFF546E6E)
+
+val PrimaryDark = Color(0xFF80CBC4)
+val OnPrimaryDark = Color(0xFF004D40)
+val PrimaryContainerDark = Color(0xFF00695C)
+val SecondaryDark = Color(0xFF4DB6AC)
+val SurfaceDark = Color(0xFF111818)
+val OnSurfaceVariantDark = Color(0xFFC5D6D6)
